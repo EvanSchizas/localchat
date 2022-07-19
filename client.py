94b6cac710a8d3238bc345ad6124c639c1b1a7de
Evan Schizas <evan.schizas.learning@gmail.com>
2022-07-19 05:29:42 +0300

    Created the server handling and midway through the gui for chat interaction

---
+++ client.py
@@ -0,0 +1,76 @@
+import socket
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+HOST = "127.0.0.1"
+PORT = 9090
+
+class Client:
+
+    def __init__(self, host, port):
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname:", parent=msg)
+        self.gui_done = False
+
+        self.running = True
+
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive_thread)
+
+        gui_thread.start()
+        receive_thread.start()
+
+        def gui_loop(self):
+            self.win = tkinter.Tk()
+
+            self.chat_label = tkinter.Label(self.win, text="Chat:")
+
+            self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+            self.text_area.config(state="disabled")
+
+            self.msg_label = tkinter.Label(self.win, text="Message:")
+
+            self.input_area = tkinter.Text(self.win, height=3)
+
+            self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+
+            self.gui_done = True
+            self.win.protocol("WM_DELETE_WINDOW", self.stop)
+            self.win.mainloop()
+        
+
+        def stop(self):
+            self.running = False
+            self.win.destroy()
+            self.sock.close()
+            exit(0)
+
+
+
+        def write(self):
+            message = f"{self.nickname}: {self.input_area.get("1.0", "end")}"
+            self.sock.send(message.encode("utf-8"))
+            self.input_area.delete("1.0", "end")
+
+
+
+
+
+        def receive_thread(self):
+            while self.running:
+                try:
+                    message= self.sock.recv(1024)
+                    if message == "NICK":
+                        self.sock.send(self.nickname.encode("utf-8"))
+                    else:
+                        
+                except:
+                    pass
