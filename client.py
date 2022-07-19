b113e99d3a589c418aed9f1fc627d1af463d3643
EvanSchizas <evan.schizas.learning@gmail.com>
2022-07-19 09:36:57 +0300

    Finished client and fixed some syntax errors

--- client.py
+++ client.py
@@ -71,6 +71,17 @@ class Client:
                     if message == "NICK":
                         self.sock.send(self.nickname.encode("utf-8"))
                     else:
-                        
-                except:
-                    pass
+                        if self.gui_done:
+                            self.text_area.config(state="normal")
+                            self.text_area.insert("end", message)
+                            self.text_area.yview("end")
+                            self.text_area.config(state="disabled")
+
+                except ConnectionAbortedError:
+                    break
+                except : 
+                    print("Error")
+                    self.sock.close()
+                    break
+
+client = Client(HOST, PORT)
--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from http import client
 import socket
 import threading
 
-HOST = '127.0.0.1'
-PORT = '9090'
+HOST = "127.0.0.1"
+PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
